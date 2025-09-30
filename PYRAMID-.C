e42181f6ab3fac20989bf438334db929deaa2b1d
virajumaretiya-cmd <virajumaretiya@gmail.com>
2025-09-30 20:01:06 +0530

    Add files via upload

---
+++ PYRAMID-.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j,k,row,number=1;
+	clrscr();
+	printf("\n Enter Row:-");
+	scanf("%d",&row);
+	for(i=1;i<=row;i++)
+	{
+	for(k=1;k<=row-i;k++)
+	{
+	printf(" ");
+	for(j=1;j<=i;j++)
+	{
+	printf(" %d ",j);
+	}
+	}
+	printf("\n\n");
+	}
+	getch();
+}
